fcb9da4ad5f57cabc997fe440d448ec256ce1555
Beat Hagenlocher <bah@posteo.de>
2023-05-30 13:56:01 +0200

    Image upload on registration working

--- lib/tc_web/live/user_registration_live.ex
+++ lib/tc_web/live/user_registration_live.ex
@@ -21,6 +21,7 @@ defmodule TcWeb.UserRegistrationLive do
       <.simple_form
         for={@form}
         id="registration_form"
+        multipart
         phx-submit="save"
         phx-change="validate"
         phx-trigger-action={@trigger_submit}
@@ -31,13 +32,29 @@ defmodule TcWeb.UserRegistrationLive do
           Oops, something went wrong! Please check the errors below.
         </.error>
 
+        <.input field={@form[:name]} type="text" label="Name" required />
         <.input field={@form[:email]} type="email" label="Email" required />
         <.input field={@form[:password]} type="password" label="Password" required />
+
+        <div phx-drop-target={ @uploads.image.ref }>
+          <label>Avatar</label>
+            <.live_file_input upload={ @uploads.image} />
+        </div>
 
         <:actions>
           <.button phx-disable-with="Creating account..." class="w-full">Create an account</.button>
         </:actions>
       </.simple_form>
+
+      <%= for image <- @uploads.image.entries do %>
+        <div class="mt-4">
+          <.live_img_preview entry={image} width="60" />
+        </div>
+        <progress value={image.progress} max="100" />
+        <%= for err <- upload_errors(@uploads.image, image) do %>
+          <.error><%= err %></.error>
+        <% end %>
+      <% end %>
     </div>
     """
   end
@@ -49,11 +66,18 @@ defmodule TcWeb.UserRegistrationLive do
       socket
       |> assign(trigger_submit: false, check_errors: false)
       |> assign_form(changeset)
+      |> allow_upload(:image,
+        accept: ~w(.jpg .jpeg .png),
+        max_entries: 1,
+        max_file_size: 9_000_000,
+        auto_upload: true)
 
     {:ok, socket, temporary_assigns: [form: nil]}
   end
 
   def handle_event("save", %{"user" => user_params}, socket) do
+    user_params = params_with_image(socket, user_params)
+
     case Accounts.register_user(user_params) do
       {:ok, user} ->
         {:ok, _} =
@@ -83,5 +107,23 @@ defmodule TcWeb.UserRegistrationLive do
     else
       assign(socket, form: form)
     end
+  end
+
+  def params_with_image(socket, params) do
+    path =
+      socket
+      |> consume_uploaded_entries(:image, &upload_static_file/2)
+      |> List.first()
+
+    Map.put(params, "avatar_upload", path)
+  end
+
+  defp upload_static_file(%{path: path}, _entry) do
+    # Real image persistence
+    filename = Path.basename(path)
+    dest = Path.join("priv/static/images", filename)
+    File.cp!(path, dest)
+
+    {:ok, ~p"/images/#{filename}"}
   end
 end
