8dd7777290fe3d509c3b4f7a812126c4eb505df8
Beat Hagenlocher <bah@posteo.de>
2023-05-30 14:51:08 +0200

    Add the option to change the username

--- lib/tc_web/live/user_settings_live.ex
+++ lib/tc_web/live/user_settings_live.ex
@@ -23,6 +23,28 @@ defmodule TcWeb.UserSettingsLive do
     <div class="space-y-12 divide-y">
       <div>
         <.simple_form
+          for={@name_form}
+          id="name_form"
+          phx-submit="update_name"
+          phx-change="validate_name"
+        >
+          <.input field={@name_form[:name]} type="text" label="name" required />
+          <.input
+            field={@name_form[:current_password]}
+            name="current_password"
+            id="current_password_for_name"
+            type="password"
+            label="Current password"
+            value={@name_form_current_password}
+            required
+          />
+          <:actions>
+            <.button phx-disable-with="Changing...">Change Email</.button>
+          </:actions>
+        </.simple_form>
+      </div>
+      <div>
+        <.simple_form
           for={@email_form}
           id="email_form"
           phx-submit="update_email"
@@ -98,19 +120,50 @@ defmodule TcWeb.UserSettingsLive do
 
   def mount(_params, _session, socket) do
     user = socket.assigns.current_user
+    name_changeset = Accounts.change_user_name(user)
     email_changeset = Accounts.change_user_email(user)
     password_changeset = Accounts.change_user_password(user)
 
     socket =
       socket
       |> assign(:current_password, nil)
+      |> assign(:name_form_current_password, nil)
       |> assign(:email_form_current_password, nil)
+      |> assign(:current_name, user.name)
       |> assign(:current_email, user.email)
+      |> assign(:name_form, to_form(name_changeset))
       |> assign(:email_form, to_form(email_changeset))
       |> assign(:password_form, to_form(password_changeset))
       |> assign(:trigger_submit, false)
 
     {:ok, socket}
+  end
+
+  def handle_event("validate_name", params, socket) do
+    %{"current_password" => password, "user" => user_params} = params
+
+    name_form =
+      socket.assigns.current_user
+      |> Accounts.change_user_name(user_params)
+      |> Map.put(:action, :validate)
+      |> to_form()
+
+    {:noreply, assign(socket, name_form: name_form, name_form_current_password: password)}
+  end
+
+  def handle_event("update_name", params, socket) do
+    %{"current_password" => password, "user" => user_params} = params
+    user = socket.assigns.current_user
+
+    case Accounts.update_user_name(user, password, user_params) do
+      {:ok, _updated_user} ->
+
+        info = "You have successfully changed your username"
+        {:noreply, socket |> put_flash(:info, info) |> assign(name_form_current_password: nil)}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, :name_form, to_form(Map.put(changeset, :action, :insert)))}
+    end
   end
 
   def handle_event("validate_email", params, socket) do
