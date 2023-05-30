eaf37264919367bd640ee27e67a7f19aba20486e
Beat Hagenlocher <bah@posteo.de>
2023-05-30 17:55:21 +0200

    Add profile_live view

---
+++ lib/tc_web/live/profile_live.ex
@@ -0,0 +1,29 @@
+defmodule TcWeb.ProfileLive do
+  use TcWeb, :live_view
+
+  alias Tc.Accounts
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <h2 class="text-2xl">
+        <%= @profile.name %> | <%= @profile.email %>
+      </h2>
+      <div class="my-5">
+      <%= if @profile.avatar_upload do %>
+        <img src={@profile.avatar_upload}/>
+      <% end %>
+      </div>
+    </div>
+    """
+  end
+
+  def mount(%{"user_name" => user_name} = params, _session, socket) do
+    profile = Accounts.get_user_by_name(user_name)
+
+    {:ok,
+    socket
+    |> assign(profile: profile)
+    }
+  end
+end
