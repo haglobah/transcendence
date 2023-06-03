3d11506b6b00d3e8d5ec9171a9c1ff1d9c730354
Beat Hagenlocher <bah@posteo.de>
2023-06-03 17:13:42 +0200

    Add home route when authenticated

---
+++ lib/tc_web/live/home_live.ex
@@ -0,0 +1,21 @@
+defmodule TcWeb.HomeLive do
+  use TcWeb, :live_view
+
+  alias Tc.Accounts
+
+  @moduledoc """
+  The home page one accesses when logged in.
+  """
+
+  def render(assigns) do
+    ~H"""
+    <div class="text-2xl my-10">
+      <h2>This is <%= @current_user.name %>'s Home Page</h2>
+    </div>
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+end
--- lib/tc_web/router.ex
+++ lib/tc_web/router.ex
@@ -17,6 +17,52 @@ defmodule TcWeb.Router do
     plug :accepts, ["json"]
   end
 
+
+  ## Authentication routes
+
+  scope "/", TcWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{TcWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", TcWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{TcWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+
+      live "/", HomeLive
+      live "/game", GameLive
+
+      live "/:user_name", ProfileLive
+    end
+  end
+
+  scope "/", TcWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{TcWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
+    end
+  end
+
+  ## Standard Routes
+
   scope "/", TcWeb do
     pipe_through :browser
 
@@ -52,48 +98,6 @@ defmodule TcWeb.Router do
 
       live_dashboard "/dashboard", metrics: TcWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
-  end
-
-  ## Authentication routes
-
-  scope "/", TcWeb do
-    pipe_through [:browser, :redirect_if_user_is_authenticated]
-
-    live_session :redirect_if_user_is_authenticated,
-      on_mount: [{TcWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
-      live "/users/log_in", UserLoginLive, :new
-      live "/users/reset_password", UserForgotPasswordLive, :new
-      live "/users/reset_password/:token", UserResetPasswordLive, :edit
-    end
-
-    post "/users/log_in", UserSessionController, :create
-  end
-
-  scope "/", TcWeb do
-    pipe_through [:browser, :require_authenticated_user]
-
-    live_session :require_authenticated_user,
-      on_mount: [{TcWeb.UserAuth, :ensure_authenticated}] do
-      live "/users/settings", UserSettingsLive, :edit
-      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
-
-      live "/game", GameLive
-
-      live "/:user_name", ProfileLive
-    end
-  end
-
-  scope "/", TcWeb do
-    pipe_through [:browser]
-
-    delete "/users/log_out", UserSessionController, :delete
-
-    live_session :current_user,
-      on_mount: [{TcWeb.UserAuth, :mount_current_user}] do
-      live "/users/confirm/:token", UserConfirmationLive, :edit
-      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
