5d05f26cfe3a2c21c232ca1a717ed287790d1493
Beat Hagenlocher <bah@posteo.de>
2023-06-06 12:03:05 +0200

    Change everything to %{x: x, y: y} maps

--- lib/tc/game/paddle.ex
+++ lib/tc/game/paddle.ex
@@ -2,28 +2,28 @@ defmodule Tc.Game.Paddle do
 
   @movement_speed 1/3000000000
 
-  defstruct [:position, :velocity, :width, :height]
+  defstruct [:pos, :velocity, :width, :height]
 
   def new do
     %__MODULE__{
-      position: {0, 37},
-      velocity: {0, 0},
+      pos: %{x: 0, y: 37},
+      velocity: %{x: 0, y: 0},
       width: 2,
       height: 25,
     }
   end
 
-  def put_velocity(paddle, {vx, vy}) do
-    update_in(paddle.velocity, fn {_vx, _vy} -> {vx, vy} end)
+  def put_velocity(paddle, %{x: vx, y: vy}) do
+    update_in(paddle.velocity, fn %{x: _vx, y: _vy} -> %{x: vx, y: vy} end)
   end
 
   def move(paddle, dt) do
-    update_in(paddle.position, fn {x, y} ->
-      {_vx, vy} = paddle.velocity
+    update_in(paddle.pos, fn %{x: x, y: y} ->
+      %{x: _vx, y: vy} = paddle.velocity
       # x2 = x + vx * dt * @movement_speed
       y2 = y + vy * dt * @movement_speed
 
-      {x, y2}
+      %{x: x, y: y2}
     end)
   end
 end
--- lib/tc/game/state.ex
+++ lib/tc/game/state.ex
@@ -7,6 +7,12 @@ defmodule Tc.Game.State do
     [:start_time, :time, :player_left, :player_right, :paddle_left, :paddle_right, :ball, :score]
   """
 
+  @max_paddle_y 75
+  @min_paddle_y 0
+
+  @max_ball 98
+  @min_ball 0
+
   defstruct [:game_id,
     :start_time, :time,
     :player_left, :player_right,
@@ -24,7 +30,7 @@ defmodule Tc.Game.State do
       paddle_left: Paddle.new(),
       paddle_right: Paddle.new(),
       ball: Ball.new(),
-      score: {0, 0},
+      score: %{left: 0, right: 0},
     }
   end
 
@@ -34,7 +40,25 @@ defmodule Tc.Game.State do
               paddle_left: Paddle.move(state.paddle_left, dt),
               paddle_right: Paddle.move(state.paddle_right, dt),
             }
-    |> check_collisions()
+    # |> enforce_boundaries()
+    # |> check_collisions()
+  end
+
+  def enforce_boundaries(state) do
+    %{x: _lx, y: ly} = state.paddle_left.pos
+    %{x: _rx, y: ry} = state.paddle_right.pos
+    %{x: bx, y: by} = state.ball.pos
+
+    if ly < @min_paddle_y, do: put_in(state.paddle_left.pos.y, @min_paddle_y)
+    if ly > @max_paddle_y, do: put_in(state.paddle_left.pos.y, @max_paddle_y)
+
+    if ry < @min_paddle_y, do: put_in(state.paddle_right.pos.y, @min_paddle_y)
+    if ry > @max_paddle_y, do: put_in(state.paddle_right.pos.y, @max_paddle_y)
+
+    if bx < @min_ball, do: put_in(state.ball.pos.x, @min_ball)
+    if bx > @max_ball, do: put_in(state.ball.pos.x, @max_ball)
+    if by < @min_ball, do: put_in(state.ball.pos.y, @min_ball)
+    if by > @max_ball, do: put_in(state.ball.pos.y, @max_ball)
   end
 
   def check_collisions(state) do
