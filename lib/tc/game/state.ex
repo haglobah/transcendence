6d3b6f17affe406ac7228d360b11e5b3ec8d1459
Beat Hagenlocher <bah@posteo.de>
2023-06-06 12:04:48 +0200

    Rename paddle_* to *

--- lib/tc/game/state.ex
+++ lib/tc/game/state.ex
@@ -4,7 +4,7 @@ defmodule Tc.Game.State do
 
   @moduledoc """
   Keeps track of:
-    [:start_time, :time, :player_left, :player_right, :paddle_left, :paddle_right, :ball, :score]
+    [:start_time, :time, :player_left, :player_right, :left, :right, :ball, :score]
   """
 
   @max_paddle_y 75
@@ -16,7 +16,7 @@ defmodule Tc.Game.State do
   defstruct [:game_id,
     :start_time, :time,
     :player_left, :player_right,
-    :paddle_left, :paddle_right,
+    :left, :right,
     :ball, :score]
 
   def new(player_left, player_right, game_id) do
@@ -27,8 +27,8 @@ defmodule Tc.Game.State do
       time: start_time,
       player_left: player_left,
       player_right: player_right,
-      paddle_left: Paddle.new(),
-      paddle_right: Paddle.new(),
+      left: Paddle.new(),
+      right: Paddle.new(),
       ball: Ball.new(),
       score: %{left: 0, right: 0},
     }
@@ -37,23 +37,23 @@ defmodule Tc.Game.State do
   def tick(state, new_time, dt) do
     %{state | time: new_time,
               ball: Ball.move(state.ball, dt),
-              paddle_left: Paddle.move(state.paddle_left, dt),
-              paddle_right: Paddle.move(state.paddle_right, dt),
+              left: Paddle.move(state.left, dt),
+              right: Paddle.move(state.right, dt),
             }
     # |> enforce_boundaries()
     # |> check_collisions()
   end
 
   def enforce_boundaries(state) do
-    %{x: _lx, y: ly} = state.paddle_left.pos
-    %{x: _rx, y: ry} = state.paddle_right.pos
+    %{x: _lx, y: ly} = state.left.pos
+    %{x: _rx, y: ry} = state.right.pos
     %{x: bx, y: by} = state.ball.pos
 
-    if ly < @min_paddle_y, do: put_in(state.paddle_left.pos.y, @min_paddle_y)
-    if ly > @max_paddle_y, do: put_in(state.paddle_left.pos.y, @max_paddle_y)
+    if ly < @min_paddle_y, do: put_in(state.left.pos.y, @min_paddle_y)
+    if ly > @max_paddle_y, do: put_in(state.left.pos.y, @max_paddle_y)
 
-    if ry < @min_paddle_y, do: put_in(state.paddle_right.pos.y, @min_paddle_y)
-    if ry > @max_paddle_y, do: put_in(state.paddle_right.pos.y, @max_paddle_y)
+    if ry < @min_paddle_y, do: put_in(state.right.pos.y, @min_paddle_y)
+    if ry > @max_paddle_y, do: put_in(state.right.pos.y, @max_paddle_y)
 
     if bx < @min_ball, do: put_in(state.ball.pos.x, @min_ball)
     if bx > @max_ball, do: put_in(state.ball.pos.x, @max_ball)
