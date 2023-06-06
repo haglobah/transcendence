5dded64a8bfefadf2b55e016666a959a67ae4ed7
Beat Hagenlocher <bah@posteo.de>
2023-06-06 11:09:22 +0200

    Rm unneeded things

--- lib/tc/game/paddle.ex
+++ lib/tc/game/paddle.ex
@@ -13,13 +13,6 @@ defmodule Tc.Game.Paddle do
     }
   end
 
-def change_position(state, :paddle_right, {dx, dy} = _change) do
-    update_in(state.paddle_right.position, fn {x, y} -> {x + dx, y + dy} end)
-  end
-  def change_position(state, :paddle_left, {dx, dy} = _change) do
-    update_in(state.paddle_left.position, fn {x, y} -> {x + dx, y + dy} end)
-  end
-
   def put_velocity(paddle, {vx, vy}) do
     update_in(paddle.velocity, fn {_vx, _vy} -> {vx, vy} end)
   end
