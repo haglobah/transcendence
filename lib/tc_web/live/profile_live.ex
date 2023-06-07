743508de762cb3ce239ef36acfb301d90d5b9221
Beat Hagenlocher <bah@posteo.de>
2023-06-07 18:44:31 +0200

    Add showing matches, preloads are still missing

--- lib/tc_web/live/profile_live.ex
+++ lib/tc_web/live/profile_live.ex
@@ -2,6 +2,7 @@ defmodule TcWeb.ProfileLive do
   use TcWeb, :live_view
 
   alias Tc.Accounts
+  alias Tc.Stats
 
   def render(assigns) do
     ~H"""
@@ -14,16 +15,24 @@ defmodule TcWeb.ProfileLive do
         <img src={@profile.avatar_upload}/>
       <% end %>
       </div>
+      <.table id="matches" rows={@matches}>
+        <:col :let={match} label="player_left"><%= match.player_left.name %></:col>
+        <:col :let={match} label="score_left"><%= match.score_left %></:col>
+        <:col :let={match} label="score_right"><%= match.score_right %></:col>
+        <:col :let={match} label="player_right"><%= match.player_right.name %></:col>
+      </.table>
     </div>
     """
   end
 
   def mount(%{"user_name" => user_name}, _session, socket) do
     profile = Accounts.get_user_by_name(user_name)
+    matches = Stats.list_matches_for_user(profile.id)
 
     {:ok,
     socket
     |> assign(profile: profile)
+    |> assign(matches: matches)
     }
   end
 end
