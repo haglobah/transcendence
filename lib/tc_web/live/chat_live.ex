e2b714b89f8b971e2c8a8d314e844ccfdf35f571
Beat Hagenlocher <bah@posteo.de>
2023-06-14 15:56:10 +0200

    Add barebone chat live_view

---
+++ lib/tc_web/live/chat_live.ex
@@ -0,0 +1,30 @@
+defmodule TcWeb.ChatLive do
+  use TcWeb, :live_view
+
+
+  def render(assigns) do
+    ~H"""
+    <div class="flex">
+      <div class="h-[88vh] sticky top-14 w-52 overflow-y-scroll hidden md:block">
+        <h3 class="text-xl">
+          Here comes the chat list.
+        </h3>
+      </div>
+    </div>
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok,
+      socket
+      # |> assign(rooms: [room])
+    }
+  end
+
+  def mount(%{"room_id" => room_id}, _session, socket) do
+    {:ok,
+      socket
+      |> assign(active_room: room_id)
+    }
+  end
+end
