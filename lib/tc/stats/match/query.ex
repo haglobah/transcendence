5ee2fd67b9e9710e16e567803554cbac7f827dcd
Beat Hagenlocher <bah@posteo.de>
2023-06-14 14:39:42 +0200

    Add _very inefficient_ way to display everything, preloads missing

--- lib/tc/stats/match/query.ex
+++ lib/tc/stats/match/query.ex
@@ -6,9 +6,14 @@ defmodule Tc.Stats.Match.Query do
     Match
   end
 
+  # defp preload_user(user_id) do
+  #   from u in User, where: u.id == ^user_id
+  # end
+
   def for_user(query \\ base(), user_id) do
     query
     |> where([m], m.player_left_id == ^user_id or m.player_right_id == ^user_id)
     |> order_by([m], {:asc, m.inserted_at})
+    # |> preload([m], [:match, player_left: ^preload_user(m.player_left_id)])
   end
 end
