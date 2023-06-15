805f70348750c9369ec048f53fe34a7ff72eb75d
Beat Hagenlocher <bah@posteo.de>
2023-06-15 15:14:29 +0200

    Rename id to room_id

--- lib/tc/chat/room.ex
+++ lib/tc/chat/room.ex
@@ -4,15 +4,15 @@ defmodule Tc.Chat.Room do
 
   alias Tc.Accounts.User
 
-  @primary_key {:id, :binary_id, autogenerate: true}
+  @primary_key {:room_id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "rooms" do
     field :description, :string
     field :name, :string
-    belongs_to :owner, User, foreign_key: :user_id
-    has_many :admins, User, foreign_key: :user_id
-    has_many :members, User, foreign_key: :user_id
-    has_many :blocked, User, foreign_key: :user_id
+    belongs_to :user, User
+    has_many :admins, User, foreign_key: :id
+    has_many :members, User, foreign_key: :id
+    has_many :blocked, User, foreign_key: :id
 
     timestamps()
   end
@@ -20,7 +20,7 @@ defmodule Tc.Chat.Room do
   @doc false
   def changeset(room, attrs) do
     room
-    |> cast(attrs, [:name, :description])
-    |> validate_required([:name, :description])
+    |> cast(attrs, [:name, :description, :owner])
+    |> validate_required([:name, :description, :owner])
   end
 end
