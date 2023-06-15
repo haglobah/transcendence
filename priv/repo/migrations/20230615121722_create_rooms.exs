b42913b18d4f07c223158765a5ef112f18602211
Beat Hagenlocher <bah@posteo.de>
2023-06-15 15:51:36 +0200

    Put all (test) migrations into one

--- priv/repo/migrations/20230615121722_create_rooms.exs
+++ priv/repo/migrations/20230615121722_create_rooms.exs
@@ -3,9 +3,10 @@ defmodule Tc.Repo.Migrations.CreateRooms do
 
   def change do
     create table(:rooms, primary_key: false) do
-      add :id, :binary_id, primary_key: true
+      add :room_id, :binary_id, primary_key: true
       add :name, :string
       add :description, :string
+      add :owner_id, references(:users, type: :binary_id), null: false
 
       timestamps()
     end
