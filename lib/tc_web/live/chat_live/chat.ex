8280995c440a7d841e455e79c3ffc179abe39f00
Beat Hagenlocher <bah@posteo.de>
2023-06-18 19:28:34 +0200

    Adjust styling of messages

--- lib/tc_web/live/chat_live/chat.ex
+++ lib/tc_web/live/chat_live/chat.ex
@@ -16,24 +16,24 @@ defmodule TcWeb.ChatLive.Chat do
       <%= @page %>
     </span>
 
+    <div :if={@start_of_messages?} class="mt-5 text-[50px] text-center">
+      🎉 You made it to the beginning of time 🎉
+    </div>
     <ul
       id="activity"
       phx-update="stream"
-      phx-viewport-top={@page > 1 && "prev-page"}
-      phx-viewport-bottom={!@start_of_messages? && "next-page"}
+      phx-viewport-top={!@start_of_messages? && "next-page"}
+      phx-viewport-bottom={@page > 1 && "prev-page"}
       phx-page-loading
       class={[
-        if(@start_of_messages?, do: "pb-10", else: "pb-[calc(200vh)]"),
-        if(@page == 1, do: "pt-10", else: "pt-[calc(200vh)]")
+        if(@start_of_messages?, do: "pb-10", else: "pt-[calc(50vh)]"),
+        if(@page == 1, do: "pt-10", else: "pb-[calc(50vh)]")
       ]}
     >
       <li :for={{id, message} <- @messages} id={id}>
         <.message message={message} />
       </li>
     </ul>
-    <div :if={@start_of_messages?} class="mt-5 text-[50px] text-center">
-      🎉 You made it to the beginning of time 🎉
-    </div>
     """
   end
 
@@ -49,9 +49,9 @@ defmodule TcWeb.ChatLive.Chat do
     <dl class="-my-4 divide-y divide-zinc-100">
       <div class="flex gap-4 py-4 sm:gap-2">
         <%!-- <.user_icon /> --%>
-        <dt class="w-1/8 flex-none text-[0.9rem] leading-8 text-zinc-500" style="font-weight: 900">
+        <dt class="w-1/8 flex-none text-[0.9rem] leading-8 text-zinc-500">
           <%= @message.sender_id %>
-          <span style="font-weight: 300">[<%= @message.inserted_at %>]</span>
+          <span>[<%= @message.inserted_at %>]</span>
           <%!-- <.delete_icon id={"message-#{@message.id}-buttons"} phx_click="delete_message" value={@message.id} /> --%>
         </dt>
       </div>
@@ -63,7 +63,7 @@ defmodule TcWeb.ChatLive.Chat do
     ~H"""
     <dl class="-my-4 divide-y divide-zinc-100">
       <div class="flex gap-4 py-4 sm:gap-2">
-        <dd class="text-sm leading-10 text-zinc-700" style="margin-left: 3%;margin-top: -1%;">
+        <dd class="text-sm leading-10 text-zinc-700">
           <%= @message.content %>
         </dd>
       </div>
