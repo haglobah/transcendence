8fbe0ac25892d816da75ee9941e948299c1b7338
Beat Hagenlocher <bah@posteo.de>
2023-06-18 16:57:25 +0200

    Add rudimetary room_list

--- lib/tc_web/live/chat_live.ex
+++ lib/tc_web/live/chat_live.ex
@@ -1,30 +1,40 @@
 defmodule TcWeb.ChatLive do
   use TcWeb, :live_view
+  alias Tc.Chat
 
+  import TcWeb.ChatLive.Component
 
   def render(assigns) do
     ~H"""
-    <div class="flex">
-      <div class="h-[88vh] sticky top-14 w-52 overflow-y-scroll hidden md:block">
-        <h3 class="text-xl">
-          Here comes the chat list.
-        </h3>
-      </div>
+    <div id="mobile-sidenav" class="fixed bg-white overflow-y-scroll block md:hidden z-50 inset-0">
+      <.room_list rooms={ @rooms }/>
     </div>
+    <aside class="h-[88vh] sticky top-14 w-52 overflow-y-scroll hidden md:block">
+      <.room_list rooms={ @rooms }/>
+    </aside>
     """
   end
 
-  def mount(%{"room_id" => room_id}, _session, socket) do
-    {:ok,
-      socket
-      |> assign(active_room: room_id)
-    }
-  end
+  # def mount(%{"room_id" => room_id}, _session, socket) do
+  #   {:ok,
+  #     socket
+  #     |> assign(active_room: room_id)
+  #   }
+  # end
 
   def mount(_params, _session, socket) do
     {:ok,
       socket
-      # |> assign(rooms: [room])
+      |> assign(rooms: Chat.list_rooms())
     }
   end
+
+  def handle_params(%{"room_id" => room_id}, _session, %{assigns: assigns} = socket) do
+    {:noreply,
+    socket
+    |> assign(rooms: assigns.rooms)
+    |> assign(active_room: room_id)
+    }
+  end
+  def handle_params(_params, _session, socket), do: {:noreply, socket}
 end
---
+++ lib/tc_web/live/chat_live/component.ex
@@ -0,0 +1,21 @@
+defmodule TcWeb.ChatLive.Component do
+  use Phoenix.Component
+  use TcWeb, :html
+
+  attr :rooms, :list
+  def room_list(assigns) do
+    ~H"""
+    <div class="bg-sky-200">
+      <%= for room <- @rooms do %>
+        <.link patch={~p"/chat/rooms/#{room.id}"}
+               class={"flex items-center p-2"}
+        >
+          <%!-- <.chat_icon /> --%>
+          <h3 class="ml-3"><%= room.name %></h3>
+          <p class="text-sm ml-3"><%= room.description %></p>
+        </.link>
+      <% end %>
+    </div>
+    """
+  end
+end
