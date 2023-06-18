c9f6e1fd433ef538224c169aecf0a89d3e47e44e
Beat Hagenlocher <bah@posteo.de>
2023-06-18 18:15:50 +0200

    Add very basic rendering of msgs via stream

--- lib/tc_web/live/chat_live.ex
+++ lib/tc_web/live/chat_live.ex
@@ -3,6 +3,22 @@ defmodule TcWeb.ChatLive do
   alias Tc.Chat
 
   import TcWeb.ChatLive.Component
+  import TcWeb.ChatLive.Chat
+
+  def render(%{live_action: :show} = assigns) do
+    ~H"""
+    <div id="mobile-sidenav" class="fixed bg-white overflow-y-scroll block md:hidden z-50 inset-0">
+      <.room_list rooms={ @rooms }/>
+    </div>
+    <aside class="h-[88vh] sticky top-14 w-52 overflow-y-scroll hidden md:block">
+      <.room_list rooms={ @rooms }/>
+    </aside>
+    <.list_messages
+      messages={ @streams.messages }
+      page={ @page }
+      start_of_messages?={ @start_of_messages? }/>
+    """
+  end
 
   def render(assigns) do
     ~H"""
@@ -15,26 +31,58 @@ defmodule TcWeb.ChatLive do
     """
   end
 
-  # def mount(%{"room_id" => room_id}, _session, socket) do
-  #   {:ok,
-  #     socket
-  #     |> assign(active_room: room_id)
-  #   }
-  # end
+  def mount(%{"room_id" => room_id}, _session, socket) do
+    messages = Chat.list_messages()
+
+    {:ok,
+    socket
+    |> assign(page: 1, per_page: 20, start_of_messages?: false)
+    |> assign(active_room: room_id)
+    |> assign(rooms: Chat.list_rooms())
+    |> stream(:messages, messages)
+    # |> paginate_logs(1)
+    }
+  end
 
   def mount(_params, _session, socket) do
+    # IO.inspect(socket.assigns)
     {:ok,
       socket
       |> assign(rooms: Chat.list_rooms())
     }
   end
 
-  def handle_params(%{"room_id" => room_id}, _session, %{assigns: assigns} = socket) do
-    {:noreply,
-    socket
-    |> assign(rooms: assigns.rooms)
-    |> assign(active_room: room_id)
-    }
+  def handle_params(_params, _session, socket) do
+    {:noreply, socket}
   end
   def handle_params(_params, _session, socket), do: {:noreply, socket}
+
+  # defp paginate_msgs(socket, new_page) when new_page >= 1 do
+  #   %{active_room: room, per_page: per_page, page: cur_page} = socket.assigns
+  #   msgs = Chat.list_msgs(
+  #     room: room,
+  #     offset: (new_page - 1) * per_page,
+  #     limit: per_page
+  #   )
+
+  #   {msgs, at, limit} =
+  #     if new_page >= cur_page do
+  #       {msgs, -1, per_page * 3 * -1}
+  #     else
+  #       {Enum.reverse(msgs), 0, per_page * 3}
+  #     end
+
+  #   case msgs do
+  #     [] ->
+  #       socket
+  #       |> assign(end_of_timeline?: at == -1)
+  #       |> stream(:messages, [])
+
+  #     [_ | _] = msgs ->
+  #       socket
+  #       |> assign(end_of_timeline?: false)
+  #       |> assign(page: new_page)
+  #       |> stream(:messages, msgs, at: at, limit: limit)
+  #   end
+  # end
 end
