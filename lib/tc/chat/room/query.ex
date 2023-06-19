90965070da9e249baf4cf72c9e3bd2482a42b3da
Beat Hagenlocher <bah@posteo.de>
2023-06-19 16:32:37 +0200

    Filter view by room membership

---
+++ lib/tc/chat/room/query.ex
@@ -0,0 +1,15 @@
+defmodule Tc.Chat.Room.Query do
+  import Ecto.Query
+  alias Tc.Chat.Room
+  alias Tc.Repo
+
+  def base do
+    Room
+  end
+
+  def for_user(query \\ base(), user_id) do
+    user_id = Ecto.UUID.dump!(user_id)
+    query
+    |> where([r], ^user_id == fragment("any(?)", r.members))
+  end
+end
