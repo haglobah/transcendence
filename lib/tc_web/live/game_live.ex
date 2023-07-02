829584536d4e36a3c537c1beb320eb40c6dbcf8b
Lukas Psyk <lpsyk@3-C-1.42heilbronn.de>
2023-07-02 14:48:13 +0200

    adjusted manual

--- lib/tc_web/live/game_live.ex
+++ lib/tc_web/live/game_live.ex
@@ -15,12 +15,11 @@ defmodule TcWeb.GameLive do
     ~H"""
     <div>
       <h2 class="text-center font-mono">
-        <br><b>About the game</b>:<br>
-        Players use paddles to hit the ball back and forth.<br>
-        Points are earned when one fails to return the ball.<br><br>
-        <%!-- <b>Goal</b>: Be the <b>1st</b> to reach <b>11 points</b>.<br> --%>
-        <b>How to play</b>:<br>
-        Players move their paddle <b>up</b> and <b>down</b> using <b>↑</b> and <b>↓</b> on their keyboards.<br><br><br>
+        <p><b>About the game</b>:</p>
+        <p>Players use paddles to hit the ball back and forth.</p>
+        <p>Points are earned when one fails to return the ball.</p><br>
+        <p><b>How to play</b>:</p>
+        <p>Players move their paddle <b>up</b> and <b>down</b> using <b>↑</b> and <b>↓</b> on their keyboards.</p><br>
       </h2>
     </div>
     <.game_over view_box="0 0 100 100">
@@ -37,12 +36,15 @@ defmodule TcWeb.GameLive do
     ~H"""
     <div>
       <h2 class="text-center font-mono">
-        <br><b>About the game</b>:<br>
-        Players use paddles to hit the ball back and forth.<br>
-        Points are earned when one fails to return the ball.<br><br>
-        <%!-- <b>Goal</b>: Be the <b>1st</b> to reach <b>11 points</b>.<br> --%>
-        <b>How to play</b>:<br>
-        Players move their paddle <b>up</b> and <b>down</b> using <b>↑</b> and <b>↓</b> on their keyboards.<br><br><br>
+        <p><b>About the game</b>:</p>
+        <p>Players use paddles to hit the ball back and forth.</p>
+        <p>Points are earned when one fails to return the ball.</p><br>
+        <p><b>How to play</b>:</p>
+        <p>Players move their paddle <b>up</b> and <b>down</b> using <b>↑</b> and <b>↓</b> on their keyboards.</p><br>
+        <p><b>Additional game features</b>:</p>
+        <.button>
+          Pause Game
+        </.button><br><br>
       </h2>
     </div>
     <.canvas view_box="0 0 100 100">
